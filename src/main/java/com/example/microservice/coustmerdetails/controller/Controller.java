090a189ccf4e3d926209ca87e4ad963e72d8f564
vineshreddy845 <64718480+vineshreddy845@users.noreply.github.com>
2023-03-15 20:06:08 -0500

    used service instances to call another microservice without hardcoding url

--- src/main/java/com/example/microservice/coustmerdetails/controller/Controller.java
+++ src/main/java/com/example/microservice/coustmerdetails/controller/Controller.java
@@ -8,10 +8,13 @@ import com.example.microservice.coustmerdetails.responses.Responses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,6 +32,10 @@ public class Controller {
     private FeignClient feignClient;
     @Autowired
     private EurekaDiscoveryClient eurekaDiscoveryClient;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+    @Autowired
+    private ServiceInstance serviceInstance;
     //http://localhost:8080/details/save
     @PostMapping("/save")
     public Model save(@RequestBody Model details)
@@ -79,4 +86,27 @@ public class Controller {
 
        return  eurekaDiscoveryClient.description();
     }
+
+    //now call coustmer-bankdetails microserivce by using discovery service.
+    //above we hardcoded-right like we used in resttemplate. now we will not hardcoded.discovery-client will pick from discovery server.
+    // we added above discovery client object from spring cloud. DicoveryClient discoveryclient
+    @GetMapping("/checks/{coustmerid}/{name}")
+    public Responses response2(@PathVariable(value="coustmerid") Long coustmerid,@PathVariable(value="name")String name){
+        //Logger logger = LoggerFactory.getLogger(this.getClass());
+        // logger.info("Calling coustmer-bankdetails service with coustmerid: {}", coustmerid);
+        response.setCoustmerdetails(repo.findBycoustmerid(coustmerid));
+        response.setName("Any problem contact vinesh");
+       List<ServiceInstance> save= discoveryClient.getInstances(name);
+        System.out.println(name);
+       response.setBankdetails(restTemplate.getForObject(save.get(1).getUri()+"/bankdetails/checking/{coustmerid}", Bankdetailsresponse.class,coustmerid));
+       // right here it is happening that save.get(0)->there can be number of instance// give instance. geturi()// will give uri see below rest api ("/uri);
+        // above called like these "http://user.attlocal.net:8080"/bankdetails/checking/{coustmerid};
+        return response;
+    }
+    @GetMapping("/uri")
+    public URI uri(){
+        return serviceInstance.getUri();
+        // response is "http://user.attlocal.net:8080"
+    }
+
 }
